e0c0e4b8964a5f718172ce38e2147e55da2dbda7
dgastli <gastli.dorra@gmail.com>
2016-02-15 15:29:29 +0100

    modif

--- GRUS/bin/grus/tools/Clustering/ClusteringController.groovy
+++ GRUS/bin/grus/tools/Clustering/ClusteringController.groovy
@@ -15,15 +15,15 @@ class ClusteringController {
     
 
 	def start() {
-				
+				/*
 				Meeting meeting = Meeting.get(params.id)
 				def currentPhase= Phase.getById(meeting.process.currentPhaseId)
 				def currentTool=Tool.getById(currentPhase.currentToolId)
 				
 				if(Tool.getById(currentTool.previousToolId)!=null){
 					def previousTool=Tool.getById(currentTool.previousToolId)
-					def data=previousTool.data.getAll()
-					//def data= Data.findAll()
+					def data=previousTool.data.getAll()*/
+					def data= Data.findAll()
 					[listIdeas:data]
 				}
 				
--- GRUS/grails-app/domain/grus/tools/Brainstorming/Brainstorming.groovy
+++ GRUS/grails-app/domain/grus/tools/Brainstorming/Brainstorming.groovy
@@ -1,10 +1,22 @@
 package grus.tools.Brainstorming
+import java.util.Date;
+import java.util.UUID;
+
 import grus.Tool
 
 
 class Brainstorming extends Tool {
 		
-	static hasMany = [ ideas : Idea]
+	UUID id
+	String comment
+	String author
+	String topic
+	Date dateCreated
+	
     static constraints = {
+		id generator : "uuid2"
+		id index : true
+		comment blanck : false
+		topic blank:false
     }
 }
